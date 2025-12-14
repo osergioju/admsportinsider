ee8905a03501cc39bb42cbd1d3e3d4bb9dfb91d3
Sérgio Junior <58306459+osergioju@users.noreply.github.com>
2025-12-13 21:59:10 -0300

    Update Input.jsx

--- frontend/src/components/uxui/Input.jsx
+++ frontend/src/components/uxui/Input.jsx
@@ -4,7 +4,7 @@ export default function Input({ label, labelColor, type = "text", variant = "def
     default:
       "text-black bg-white border border-gray-300",
     dark:
-      "text-white bg-transparent border border-gray-600",
+      "text-white bg-gradient-to-l from-[#241c3a8a] to-[#29204288] border border-[#665494] rounded-full p-5",
     light:
       "text-black bg-gray-100 border border-gray-300"
   };
@@ -12,14 +12,14 @@ export default function Input({ label, labelColor, type = "text", variant = "def
   return (
     <div className="flex flex-col gap-1 mb-4">
       {label && (
-        <label className={`${labelColor} text-sm font-medium`}>
+        <label className={`${labelColor} bg-linear-to-l from-[#ffffff1f] to-[#ffffff] bg-clip-text text-lg mb-2 text-transparent`}>
           {label}
         </label>
       )}
 
       <input
         type={type}
-        className={`${variants[variant]} rounded px-3 py-2 focus:outline-none focus:ring-2 focus:ring-primary`}
+        className={`${variants[variant]} focus:outline-none focus:ring-2 focus:ring-primary`}
         {...props}
       />
     </div>
