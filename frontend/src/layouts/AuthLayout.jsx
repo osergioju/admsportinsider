e34024703cd8e9080e30fd088b02e32cd97c4550
Sérgio Junior <58306459+osergioju@users.noreply.github.com>
2025-12-14 13:14:36 -0300

    Update AuthLayout.jsx

--- frontend/src/layouts/AuthLayout.jsx
+++ frontend/src/layouts/AuthLayout.jsx
@@ -2,7 +2,7 @@ import { Outlet } from "react-router-dom";
 
 export default function AuthLayout() {
   return (
-    <div className="h-screen w-full pt-[100px] pb-[200px] bg-[#0C0718] relative overflow-hidden flex flex-col justify-center items-center">
+    <div className="min-h-screen w-full bg-[#0C0718] relative overflow-hidden flex flex-col justify-center items-center">
       <Outlet />
     </div>
   );
